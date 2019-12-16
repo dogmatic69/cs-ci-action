37e37ebd2bd06ff2e1a02e2b40df2e78ee6bd1f9
Laurea <laurea.lindroos@ikea.com>
2019-12-16 13:51:26 +0100

    First draft for action

--- files/work/entrypoint.sh
+++ files/work/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -eu
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- docker "$@"
-fi
-
-# if our command is a valid Docker subcommand, let's invoke it through Docker instead
-# (this allows for "docker run docker ps", etc)
-if docker help "$1" > /dev/null 2>&1; then
-	set -- docker "$@"
-fi
+SERVICE_PATH=$(realpath ./${INPUT_SERVICE})
+echo ${INPUT_TARGETS}
+echo ${SERVICE_PATH}
