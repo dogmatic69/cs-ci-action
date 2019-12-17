8c48399110d27694f290cac43616c17ba3d5ab7c
Laurea <laurea.lindroos@ikea.com>
2019-12-17 10:58:38 +0100

    debug

--- files/work/entrypoint.sh
+++ files/work/entrypoint.sh
@@ -5,6 +5,9 @@ SERVICE_PATH=$(realpath ./${INPUT_SERVICE})
 echo ${INPUT_TARGETS}
 echo ${SERVICE_PATH}
 
+ls -al ${SERVICE_PATH}
+ls -al ${SERVICE_PATH}../
+
 for i in ${INPUT_TARGETS[@]}; do
 	make $i
 done
