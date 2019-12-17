110edfb744c2191456dee3add7854ea2b12b2696
Laurea <laurea.lindroos@ikea.com>
2019-12-17 11:02:00 +0100

    see full path

--- files/work/entrypoint.sh
+++ files/work/entrypoint.sh
@@ -6,8 +6,8 @@ echo ${INPUT_TARGETS}
 echo ${SERVICE_PATH}
 
 ls -al ${SERVICE_PATH}
-ls -al ${SERVICE_PATH}../
+ls -al ${SERVICE_PATH}/../
 
-for i in ${INPUT_TARGETS[@]}; do
-	make $i
+for target in ${INPUT_TARGETS[@]}; do
+	make -C ${SERVICE_PATH} $target
 done
