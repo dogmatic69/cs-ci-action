03ab35ee2510c27b3bbfc500ede3997ee9d111fc
Laurea <laurea.lindroos@ikea.com>
2019-12-17 10:42:16 +0100

    added RUN in docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM alpine:3
+RUN apk add --no-cache bash make
 COPY files/ /
 RUN chmod +x /work/entrypoint.sh
 ENTRYPOINT ["/work/entrypoint.sh"]
--- files/work/entrypoint.sh
+++ files/work/entrypoint.sh
@@ -5,7 +5,6 @@ SERVICE_PATH=$(realpath ./${INPUT_SERVICE})
 echo ${INPUT_TARGETS}
 echo ${SERVICE_PATH}
 
-for i in ${INPUT_TARGETS[@]}
-do
+for i in ${INPUT_TARGETS[@]}; do
 	make $i
 done
